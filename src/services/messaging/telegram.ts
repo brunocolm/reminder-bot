be216abe071492038a26a517bbf289ae0a6394fb
Bruno Colmenares <brunocolmenaress94@gmail.com>
2025-02-06 14:21:01 +0100

    remove button after pressing

--- src/services/messaging/telegram.ts
+++ src/services/messaging/telegram.ts
@@ -3,9 +3,10 @@ export interface InlineKeyboard {
   callback_data: string;
 }
 
+const BASE_URL = `https://api.telegram.org/bot${process.env.TELEGRAM_API_KEY}`;
+
 export const sendTelegramMessage = async (chatId: number, message: string, buttons?: InlineKeyboard[]): Promise<string> => {
   console.log("Sending telegram MSG to: ", chatId)
-  const BASE_URL = `https://api.telegram.org/bot${process.env.TELEGRAM_API_KEY}`;
 
   const response = await fetch(BASE_URL + "/sendMessage", {
     method: "POST",
@@ -31,9 +32,29 @@ export const createTelegramInterfaceButtons = (numberOfButtons: number): InlineK
   let inlineKeyboard: InlineKeyboard[] = []
   for (let index = 0; index < numberOfButtons; index++) {
     inlineKeyboard[index] = {
-      text: (index+1).toString(),
-      callback_data: `btn_${(index+1).toString()}`
+      text: (index + 1).toString(),
+      callback_data: (index + 1).toString(),
     }
   }
   return inlineKeyboard
 }
+
+export const removeTelegramInterfaceButtonsFromMessage = async (chatId: number, messageId: number): Promise<string> => {
+  console.log("removeTelegramInterfaceButtonsFromMessage")
+  const url = BASE_URL + `/editMessageReplyMarkup`;
+
+  const body = {
+    chat_id: chatId,
+    message_id: messageId,
+    reply_markup: JSON.stringify({ inline_keyboard: [[]] })
+  };
+
+  const res = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body)
+  });
+
+  console.log("Removing telegram buttons")
+  return res.json()
+};
